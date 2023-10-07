6656201f9e3e1278640d7b9f0b52896b545722ff
Tuhin-Reza <tuhin.aiub2018@gmail.com>
2023-10-07 21:59:57 +0600

    first-commit+update

---
+++ Lab3/Controllers/FormController.cs
@@ -0,0 +1,28 @@
+﻿using Lab3.Models;
+using System.Web.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Form(Form info)
+        {
+            return View(info);
+        }
+
+        //public ActionResult Form(FormCollection fc)
+        //{
+        //    ViewBag.name = fc["name"];
+        //    ViewBag.userid = fc["userid"];
+        //    ViewBag.gender = fc["gender"];
+        //    ViewBag.profession = fc["profession"];
+        //    ViewBag.hobbies = fc["hobbies"];
+        //    return View();
+        //}
+    }
+}
---
+++ Lab3/Models/Form.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab3.Models
+{
+    public class Form
+    {
+        public string name { get; set; }
+        public int userid { get; set; }
+        public string gender { get; set; }
+        public string profession { get; set; }
+        public string[] hobbies { get; set; }
+    }
+}
