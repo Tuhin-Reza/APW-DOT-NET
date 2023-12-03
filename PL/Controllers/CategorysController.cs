3de1242496429e7bd673183f8fe59dae64eabaf9
Tuhin-Reza <tuhin.aiub2018@gmail.com>
2023-12-03 12:31:18 +0600

    start

---
+++ PL/Controllers/CategorysController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PL.Controllers
+{
+    public class CategorysController : ApiController
+    {
+        [Route("api/getCategoryTest")]
+        public HttpResponseMessage GetTest()
+        {
+            var name = "Category Controller";
+            //var name = new string[] { "Tuhin", "Reza" };
+            return Request.CreateResponse(HttpStatusCode.OK, name);
+        }
+
+    }
+}
---
+++ PL/Controllers/NewsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PL.Controllers
+{
+    public class NewsController : ApiController
+    {
+        [Route("api/getNewsTest")]
+        public HttpResponseMessage GetTest()
+        {
+            var name = "News Controller";
+            //var name = new string[] { "Tuhin", "Reza" };
+            return Request.CreateResponse(HttpStatusCode.OK, name);
+        }
+
+    }
+}
